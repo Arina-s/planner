0bd995cb1e5285a65b7168f2d6ee4281f7657585
Arina Slizh <arina_slizh@epam.com>
2022-04-07 18:21:14 +0300

    Lesson 3 Added MVC in xml

---
+++ src/main/java/com/arinahitech/planner/StartController.java
@@ -0,0 +1,14 @@
+package com.arinahitech.planner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+
+    @GetMapping("/")
+    public String start() {
+        return "index";
+    }
+
+}
