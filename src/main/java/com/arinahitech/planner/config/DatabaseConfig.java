e9f60009b542665a570bdb7723e277692295c2c7
Arina Slizh <arina_slizh@epam.com>
2022-06-17 22:40:55 +0300

    Lesson 5 Added application.properties

--- src/main/java/com/arinahitech/planner/config/DatabaseConfig.java
+++ src/main/java/com/arinahitech/planner/config/DatabaseConfig.java
@@ -3,26 +3,39 @@ package com.arinahitech.planner.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DatabaseConfig {
 
-    private static final String URL = "jdbc:postgresql://localhost:5433/planner-java-jdbc";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "123";
+    @Autowired
+    Environment environment;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
 
     @Bean
     public Connection connection() {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName(environment.getProperty("jdbc.Driver"));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
